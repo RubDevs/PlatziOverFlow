ef5bac4eb026b731f3c0404fb9dcea91c36dd45f
Ruben Hernandez <ruben.hernandez.puerta@rubdevs.com>
2020-10-23 11:16:16 -0500

    validation of user using joi

--- routes.js
+++ routes.js
@@ -1,5 +1,6 @@
 const site = require('./controllers/site')
 const user = require('./controllers/user')
+const Joi = require('joi')
 
 module.exports = [{
     method: 'GET',
@@ -26,6 +27,15 @@ module.exports = [{
 
 {
     method: 'POST',
+    options: {
+        validate: {
+            payload: {
+                name: Joi.string().required().min(3),
+                email: Joi.string().email().required(),
+                password: Joi.string().required().min(6)
+            }
+        }
+    },
     path: '/create-user',
     handler: user.create
 }]
