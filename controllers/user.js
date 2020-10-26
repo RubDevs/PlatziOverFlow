2c62ced061831b56f3e2fe754670e20868aee377
Ruben Hernandez <ruben.hernandez.puerta@rubdevs.com>
2020-10-26 16:24:52 -0600

    user Cookie implemented and logout

--- controllers/user.js
+++ controllers/user.js
@@ -18,15 +18,25 @@ async function validateUser(req,h) {
     let result
     try {
         result = await users.validate(req.payload)
+        if (!result){
+            return h.response('Usuario y/o contraseña incorrectos').code(401)
+        }
     } catch (error) {
         console.error(error)
         return h.response('Problema al validar usuario').code(500)
     }
-    return h.response(result)
+    return h.redirect('/').state('user',{
+        name: result.name,
+    })
+}
+
+function logout(req,h) {
+    return h.redirect('/login').unstate('user')
 }
 
 
 module.exports = {
     create: createUser,
-    validate: validateUser
+    validate: validateUser,
+    logout: logout
 }
--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const vision = require('@hapi/vision')
 const inert = require('inert')
 const path = require('path')
 const routes = require('./routes')
+const { encode } = require('querystring')
 
 const server = Hapi.server({
     port: process.env.PORT || 3000,
@@ -23,6 +24,12 @@ async function init(){
     try {
         await server.register(inert)
         await server.register(vision)
+
+        server.state('user',{
+            ttl: 1000 * 60 * 60 * 24 * 7,
+            isSecure: process.env.NODE_ENV === 'prod',
+            encoding: 'base64json'
+        })
 
         server.views({
             engines: {
